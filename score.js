5d97eca9114e25ab9deff2f847f1556dbb7e3ca0
unknown <gunapriya07>
2023-12-05 16:25:47 +0530

    assignment on numeron

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const playAgainBtn=document.getElementById("play-again-button");
+const scoreBoard=document.getElementById("score-board");
+let score=localStorage.getItem("score");
+
+scoreBoard.innerHTML=score;
+
+playAgainBtn.onclick=()=>{
+    window.location.href="./game.html";
+}
+
